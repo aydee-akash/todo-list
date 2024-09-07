16cfcb271a5191de9f90c91270579dad45d5c220
aydee-akash <137648244+aydee-akash@users.noreply.github.com>
2024-09-07 17:21:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
